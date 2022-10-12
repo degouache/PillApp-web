01ae55a256c30d6d9ac33ae952a0577ae9f21fc9
degouache <bcriloz@gmail.com>
2022-10-12 19:22:33 +0200

    Login almost done. Begining navigation between login-register.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'loginApplication',
+    redirectTo: 'login',
     pathMatch: 'full'
     },
     {
-    path: 'loginApplication',
+    path: 'login',
+    component: LoginComponent
+    },
+        {
+    path: 'register',
     component: RegisterComponent
     }
+
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RegisterModule } from './register/register.module';
+import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
-  declarations: [],
-  imports: [RegisterModule],
-  exports: [RegisterModule],
+  declarations: [LoginComponent],
+  imports: [RegisterModule, LoginModule],
+  exports: [RegisterModule, LoginModule],
   providers: [],
 })
 export class PagesModule {}
