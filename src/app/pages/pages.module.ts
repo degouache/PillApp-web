e80c58258c7f383cf739c9bb755b64f2e6faa237
degouache <bcriloz@gmail.com>
2022-10-12 14:51:40 +0200

    Begining working on registration form.

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RegisterModule } from './register/register.module';
+
+@NgModule({
+  declarations: [],
+  imports: [RegisterModule],
+  exports: [RegisterModule],
+  providers: [],
+})
+export class PagesModule {}
