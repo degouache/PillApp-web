fc5a4a73e079e51dda1f383a141d6dbe4c3008ee
degouache <100487173+degouache@users.noreply.github.com>
2022-11-27 17:58:56 +0100

    refactor from cards home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
-import { CardDrugComponent } from 'src/app/shared/card-drug/card-drug.component';
-import { CardRememberDateComponent } from 'src/app/shared/card-remember-date/card-remember-date.component';
-import { CardRememberDoItComponent } from 'src/app/shared/card-remember-do-it/card-remember-do-it.component';
-import { CardVitalComponent } from 'src/app/shared/card-vital/card-vital.component';
-import { CardCaredPersonHomeComponent } from 'src/app/shared/card-cared-person-home/card-cared-person-home.component';
+import { CardDrugComponent } from 'src/app/shared/card-home/card-drug/card-drug.component';
+import { CardRememberDateComponent } from 'src/app/shared/card-home/card-remember-date/card-remember-date.component';
+import { CardRememberDoItComponent } from 'src/app/shared/card-home/card-remember-do-it/card-remember-do-it.component';
+import { CardVitalComponent } from 'src/app/shared/card-home/card-vital/card-vital.component';
+import { CardCaredPersonHomeComponent } from 'src/app/shared/card-home/card-cared-person-home/card-cared-person-home.component';
 
 
 @NgModule({
--- src/app/pages/patients-all/patients-all.module.ts
+++ src/app/pages/patients-all/patients-all.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PatientsAllComponent } from './patients-all.component';
-import { CardCaredPersonListComponent } from 'src/app/shared/card-cared-person-list/card-cared-person-list.component';
+import { CardCaredPersonListComponent } from 'src/app/shared/card-home/card-cared-person-list/card-cared-person-list.component';
 import { TopNavigationComponent } from 'src/app/shared/top-navigation/top-navigation.component';
 
 const routes: Routes = [
