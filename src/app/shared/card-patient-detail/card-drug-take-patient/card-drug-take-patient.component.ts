2b08cee208351033450f64b218f7600791b21d5f
degouache <100487173+degouache@users.noreply.github.com>
2022-12-02 21:08:37 +0100

    cards with data inside patient

--- src/app/shared/card-patient-detail/card-drug-take-patient/card-drug-take-patient.component.ts
+++ src/app/shared/card-patient-detail/card-drug-take-patient/card-drug-take-patient.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-drug-take-patient',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardDrugTakePatientComponent implements OnInit {
 
+  @Input() public drug: any;
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/card-patient-detail/card-remember-date-patient/card-remember-date-patient.component.ts
+++ src/app/shared/card-patient-detail/card-remember-date-patient/card-remember-date-patient.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-remember-date-patient',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardRememberDatePatientComponent implements OnInit {
 
+  @Input() public meeting: any;
   constructor() { }
 
   ngOnInit() {
